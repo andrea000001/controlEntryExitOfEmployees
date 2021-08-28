790b14819ce525496f669eee8eb767af3c4264d9
andrea000001 <elenvera9@gmail.com>
2021-08-28 14:28:03 -0500

    Add entity

---
+++ controlEntryExitOfEmployees.Common/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace controlEntryExitOfEmployees.Common.Models
+{
+    public class Employee
+    {
+        public int EmployeId { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int Type { get; set; }
+
+        public bool IsConsolidated { get; set; }
+    }
+}
---
+++ controlEntryExitOfEmployees.Functions/Entities/EmployeeEntity.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace controlEntryExitOfEmployees.Functions.Entities
+{
+    public class EmployeeEntity : TableEntity
+    {
+        public int EmployeId { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int Type { get; set; }
+
+        public bool IsConsolidated { get; set; }
+    }
+}
